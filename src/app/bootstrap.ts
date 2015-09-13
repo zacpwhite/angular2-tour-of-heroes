b89b8bcebb595799589dd5e711132c119b16f12f
John Papa <john@johnpapa.net>
2015-09-12 20:15:43 -0400

    added the built-in uppercase pipe, a custom initCaps pipe, a filter component, and a filter service

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from 'angular2/angular2';
 import {ROUTER_BINDINGS} from 'angular2/router';
 import {HeroService} from './hero-service';
+import {FilterService} from './filter-service';
 import {AppComponent} from './app-component';
 
-bootstrap(AppComponent, [ROUTER_BINDINGS, HeroService]);
+bootstrap(AppComponent, [ROUTER_BINDINGS, FilterService, HeroService]);
--- src/app/heroes-component.ts
+++ src/app/heroes-component.ts
@@ -4,23 +4,38 @@ import {HeroService} from './hero-service';
 import {Hero} from './hero';
 import {Routes} from './route-config';
 import {HERO_DIRECTIVES} from './hero-directives';
+import {InitCapsPipe} from './init-caps-pipe'
+import {FilterTextComponent} from './filter-text-component';
+import {FilterService} from './filter-service';
 
 @Component({ selector: 'my-heroes' })
 @View({
   templateUrl: 'app/heroes-component.html',
-  directives: [HERO_DIRECTIVES],
-  styleUrls: ['app/heroes-component.css']
+  directives: [HERO_DIRECTIVES, FilterTextComponent],
+  styleUrls: ['app/heroes-component.css'],
+  pipes: [InitCapsPipe]
 })
 export class HeroesComponent {
+  public filteredHeroes: Hero[];
   private _heroes: Hero[];
-  currentHero: Hero;
+  public currentHero: Hero;
+  public filterText = '';
 
-  constructor(private _heroService: HeroService, private _router: Router) { }
+  heroesAsync: Promise<string>;
+
+  constructor(private _filterService: FilterService, private _heroService: HeroService, private _router: Router) {
+    this.filteredHeroes = this._heroes;
+    this.heroesAsync = new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve('heroes from promise');
+      }, 300)
+    });
+  }
 
   get heroes() {
     if (this._heroes) { return this._heroes; }
 
-    this._heroService.getHeroes().then(heroes => this._heroes = heroes);
+    this._heroService.getHeroes().then(heroes => this._heroes = this.filteredHeroes = heroes);
     return this._heroes;
   }
 
@@ -33,4 +48,8 @@ export class HeroesComponent {
   }
 
   onSelect(hero: Hero) { this.currentHero = hero; }
+
+  filterChanged(data: string) {
+    this.filteredHeroes = this._filterService.filter(data, ['id', 'name'], this._heroes);
+  }
 }
