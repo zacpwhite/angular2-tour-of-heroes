c09abdf5e754d1b67e41146d3278189c12454ba0
John Papa <john@johnpapa.net>
2015-09-12 20:41:01 -0400

    whitespacing

--- src/app/init-caps-pipe.ts
+++ src/app/init-caps-pipe.ts
@@ -1,6 +1,6 @@
 import {Pipe} from 'angular2/angular2';
 
-@Pipe({name: 'initCaps'})
+@Pipe({ name: 'initCaps' })
 export class InitCapsPipe {
   transform(value: string) {
     return value.toLowerCase().replace(/(?:^|\s)[a-z]/g, function(m) {
--- src/app/sort-heroes-pipe.ts
+++ src/app/sort-heroes-pipe.ts
@@ -1,11 +1,11 @@
 import {Pipe} from 'angular2/angular2';
 import {Hero} from './hero';
 
-@Pipe({name: 'sortHeroes'})
+@Pipe({ name: 'sortHeroes' })
 export class SortHeroesPipe {
   transform(value: Array<Hero>) {
     if (!value || !value.sort) { return value; }
-    
+
     return value.sort((a: Hero, b: Hero) => {
       if (a.name < b.name) { return -1; }
       if (a.name > b.name) { return 1; }
